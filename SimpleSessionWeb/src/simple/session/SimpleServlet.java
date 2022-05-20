58dfce63f552025c0c3bf615af09dfa148642d12
root <root@curtness1.fyre.ibm.com>
2022-05-19 17:32:09 -0700

    develop a simple Session App

---
+++ SimpleSessionWeb/src/simple/session/SimpleServlet.java
@@ -0,0 +1,50 @@
+package simple.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		if (session.getAttribute("count")!=null) {
+			
+			int i = (Integer) session.getAttribute("count");
+			System.out.println(session.getId() + ": count: " + i);
+			session.setAttribute("count", i+1);
+		} else {
+			System.out.println("first request. count: 0");
+			session.setAttribute("count", 1);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
