8984be6eb736d2d5988d0ead7a23a6d400b8cffb
root <root@reunited1.fyre.ibm.com>
2022-12-26 21:59:20 -0800

    develop a simple ssl client auth sample

--- SimpleSSLClientAuthWeb/src/simple/ssl/client/auth/MyMain.java
+++ SimpleSSLClientAuthWeb/src/simple/ssl/client/auth/MyMain.java
@@ -47,6 +47,8 @@ public class MyMain {
 
 
 /** 
+
+How to create a client keystore. 
 # openssl genrsa -out CA.key 4096  
 # openssl req -new -key CA.key -out CA.csr -subj "/C=US/CN=ROOT.com"
 # openssl x509 -req -in CA.csr -out CA.crt -signkey CA.key -sha256 -days 3650
