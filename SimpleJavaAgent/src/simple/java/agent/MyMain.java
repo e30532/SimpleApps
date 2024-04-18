1c37b76a26fed15a1e36922a69cf166dcdc5a57e
root <root@aaa>
2024-04-17 19:34:04 -0700

    add an example

--- SimpleJavaAgent/src/simple/java/agent/MyMain.java
+++ SimpleJavaAgent/src/simple/java/agent/MyMain.java
@@ -6,7 +6,7 @@ public class MyMain {
 
     public static void main(String[] args) {
         try {
-            VirtualMachine vm = VirtualMachine.attach("3706094");
+            VirtualMachine vm = VirtualMachine.attach("3720595");
             vm.loadAgent("/tmp/SimpleJavaAgentLib.jar", null);
             vm.detach();
         } catch (Exception e) {
--- SimpleJavaAgentLib/src/simple/java/agent/lib/MyAgent.java
+++ SimpleJavaAgentLib/src/simple/java/agent/lib/MyAgent.java
@@ -1,6 +1,12 @@
 package simple.java.agent.lib;
 
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 public class MyAgent {
 	
@@ -12,6 +18,34 @@ public class MyAgent {
     	System.out.println("Generating a javacore...");
     	com.ibm.jvm.Dump.JavaDump();
     	System.out.println("Done");
+
+    	//This is an example of how to execute WebSphere specific function.
+    	new Thread(() -> {
+            System.out.println("New thread running...");
+            Class<?>[] loadedClasses = instrumentation.getAllLoadedClasses();
+            ClassLoader wasloader = null;
+            for (Class<?> cls : loadedClasses) {
+            	if(cls.getName().equals("com.ibm.ws.runtime.WsServer")) {
+            		wasloader = cls.getClassLoader();
+                    try {
+                    	Class<?> adminServiceFactoryClass = wasloader.loadClass("com.ibm.websphere.management.AdminServiceFactory");
+                    	Method getAdminServiceMethod = adminServiceFactoryClass.getMethod("getAdminService", new Class[0]);
+                    	Object adminService = getAdminServiceMethod.invoke(null, new Object[0]);
+                    	Field mbServer = adminService.getClass().getDeclaredField("_mbServer");
+                    	mbServer.setAccessible(true);
+                    	MBeanServer mbs = (MBeanServer)mbServer.get(adminService);          	
+                    	Set<ObjectName> mbeans = mbs.queryNames(new ObjectName("WebSphere:name=ClusterMgr,type=ClusterMgr,*"), null);
+                    	for (ObjectName name: mbeans) {
+                    		mbs.invoke(name, "retrieveClusters", null, null);
+                    		
+                    	}
+                    	System.out.println(mbeans.size());
+                    }catch(Exception e) {
+                    	e.printStackTrace();
+                    }
+            	}
+            }
+        }).start();
     }
 }
 
